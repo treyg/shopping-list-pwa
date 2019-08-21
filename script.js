60162b50fd3a1925667c48e323f7a4f1be89a268
treyg21 <41927921+treyg21@users.noreply.github.com>
2019-08-20 21:20:06 -0400

    reverse checkbox/li order

--- script.js
+++ script.js
@@ -1,70 +1,62 @@
+var button = document.getElementById("enter"); //Store selected elements as variables
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
 
-
-var button = document.getElementById("enter")         //Store selected elements as variables
-var input = document.getElementById("userInput")
-var ul = document.querySelector("ul")
-
-function inputLength() {                              // Check to make sure input has a value
-     return input.value.length
+function inputLength() {
+  // Check to make sure input has a value
+  return input.value.length;
 }
 
 function createListElement() {
-        var li = document.createElement("li");       //Create li element       
-        var cb = createCheckBox();
+  var li = document.createElement("li"); //Create li element
+  var cb = createCheckBox();
 
-        li.appendChild(document.createTextNode(input.value)); // add the value of the input variable (li element) to the document at the end of ul
-        ul.appendChild(li);
-        li.appendChild(cb) //append the checkbox to the ul element
-        input.value = "";                       //Return input to empty after li is added
-        cb.addEventListener('click',() => {            //Add event listener on click to toggle css strike through class         
-                li.classList.toggle('strikethrough');  
-        })
-        
-        var hr = document.createElement('hr')
-hr.appendChild(li)
+  li.appendChild(cb); //append the checkbox to the ul element
+  li.appendChild(document.createTextNode(input.value)); // add the value of the input variable (li element) to the document at the end of ul
+  ul.appendChild(li);
+  input.value = ""; //Return input to empty after li is added
+  cb.addEventListener("click", () => {
+    //Add event listener on click to toggle css strike through class
+    li.classList.toggle("strikethrough");
+  });
 }
-
 
 function createCheckBox(checked = false) {
-        const cb = document.createElement('input')
-        cb.type = 'checkbox'
-        cb.checked = false
-        cb.classList.add('checkstyle')
-        return cb 
+  const cb = document.createElement("input");
+  cb.type = "checkbox";
+  cb.checked = false;
+  cb.classList.add("checkstyle");
+  return cb;
 }
-
-
-
-
-
 
 function addListAfterClick() {
-        if(inputLength() > 0) {            //If input length is greater than zero, run function to create list element
-            createListElement();             
-        }
+  if (inputLength() > 0) {
+    //If input length is greater than zero, run function to create list element
+    createListElement();
+  }
 }
 
-function addListAfterKeypress(event) {                              
-        if (inputLength() > 0  && event.keyCode === 13) {           //On keypress (enter), check to see if input > 0
-            createListElement();                                     // if so, create list element
-        }
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    //On keypress (enter), check to see if input > 0
+    createListElement(); // if so, create list element
+  }
 }
 
-button.addEventListener("click", addListAfterClick)           //ON click, run the function to create a list elemeent on click
+button.addEventListener("click", addListAfterClick); //ON click, run the function to create a list elemeent on click
 
-input.addEventListener("keypress", addListAfterKeypress)      //On keypress enter, run the function to create list element after pressing enter
-
+input.addEventListener("keypress", addListAfterKeypress); //On keypress enter, run the function to create list element after pressing enter
 
 //button to remove selected elements
 
+function removeElt() {
+  // create function to remove seleted items
+  var doneItems = document.getElementsByClassName("strikethrough"); // create var for all elements with strikethrough class
+  while (doneItems.length > 0) {
+    doneItems[0].parentNode.removeChild(doneItems[0]); //for eache strikethrough element with length > 0, remove
+  }
+}
 
-function removeElt() { // create function to remove seleted items
-        var doneItems = document.getElementsByClassName("strikethrough"); // create var for all elements with strikethrough class
-        while (doneItems.length > 0) {
-          doneItems[0].parentNode.removeChild(doneItems[0]); //for eache strikethrough element with length > 0, remove
-        }
-      }
+var remove = document.getElementById("removeItem"); //create variable for remove button
 
-var remove = document.getElementById("removeItem") //create variable for remove button
-
-remove.addEventListener('click', removeElt) //on click for button, run function to remove selected items
+remove.addEventListener("click", removeElt); //on click for button, run function to remove selected items
