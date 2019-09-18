0627ef4c7630fc89e9359cfc436cc24c8cf395d6
treyg21 <41927921+treyg21@users.noreply.github.com>
2019-09-18 13:18:03 -0400

    update local storage settings

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-var button = document.getElementById("enter"); //Store selected elements as variables
+//Store selected elements as variables
+var button = document.getElementById("enter"); 
 var input = document.getElementById("userInput");
-var ul = document.querySelector("ul");
+var ul = document.querySelector("#list");
 
-
-
-let list = []
-
-const listJSON = localStorage.getItem('list')
-
-if (listJSON !== null) {
-    list = JSON.parse(listJSON)
-}
 
 
 function inputLength() {
@@ -22,20 +14,22 @@ function createListElement() {
   var li = document.createElement("li"); //Create li element
   var cb = createCheckBox();
   li.appendChild(cb); //append the checkbox to the ul element
-  li.appendChild(document.createTextNode(input.value)); // add the value of the input variable (li element) to the document at the end of ul
+// add the value of the input variable (li element) to the document at the end of ul
+  li.appendChild(document.createTextNode(input.value)); 
   ul.appendChild(li);
-  input.value = ""; //Return input to empty after li is added
+  
   cb.addEventListener("click", () => {
     //Add event listener on click to toggle css strike through class
     li.classList.toggle("strikethrough");
   });
   
   // push ul text content to list array
-  list.push(li.textContent)
-
-  //Save list array to local storage
-  localStorage.setItem('list', JSON.stringify(list))
-
+  if(localStorage.list === undefined){
+    localStorage.setItem("list", input.value.trim() + ',');
+  }else{
+    localStorage.list = localStorage.list.concat(input.value.trim() + ',');
+  }
+  input.value = ""; //Return input to empty after li is added
 }
 
 
@@ -62,26 +56,55 @@ function addListAfterKeypress(event) {
     createListElement(); // if so, create list element
   }
 }
-
-button.addEventListener("click", addListAfterClick); //ON click, run the function to create a list elemeent on click
-
-input.addEventListener("keypress", addListAfterKeypress); //On keypress enter, run the function to create list element after pressing enter
+//ON click, run the function to create a list element on click
+button.addEventListener("click", addListAfterClick); 
+//On keypress enter, run the function to create list element after pressing enter
+input.addEventListener("keypress", addListAfterKeypress); 
 
 //button to remove selected elements
 
 function removeElt() {
   // create function to remove seleted items
-  var doneItems = document.getElementsByClassName("strikethrough"); // create var for all elements with strikethrough class
+  // create var for all elements with strikethrough class
+  var doneItems = document.getElementsByClassName("strikethrough"); 
+  console.log(doneItems);
+
+  /*for (let i = 0 ; i < doneItems.length ; i++) {
+      //for eache strikethrough element with length > 0, remove
+    console.log( 'removeELT' + doneItems[i].textContent); 
+    let patt = new RegExp(doneItems[i].textContent+',', "g");
+    localStorage.setItem("list", list.replace(patt, ""));
+   doneItems[i].parentNode.removeChild(doneItems[i]);
+  }*/
   while (doneItems.length > 0) {
-    doneItems[0].parentNode.removeChild(doneItems[0]); //for eache strikethrough element with length > 0, remove
+    let patt = new RegExp(doneItems[0].textContent+',', "g");
+    localStorage.setItem("list", localStorage.list.replace(patt, ""));
+    doneItems[0].parentNode.removeChild(doneItems[0]); 
+  }
+
+}
+//create variable for remove button
+var remove = document.getElementById("removeItem"); 
+//on click for button, run function to remove selected items
+remove.addEventListener("click", removeElt); 
+
+function loadFromLocaleStorage(){
+  if (localStorage.list){
+    localStorage.list.split(',').forEach(item => {
+      if (item !== ''){
+      var li = document.createElement("li"); 
+      var cb = createCheckBox();
+      li.appendChild(cb); 
+      li.appendChild(document.createTextNode(item)); 
+      ul.appendChild(li);
+      cb.addEventListener("click", () => {
+        li.classList.toggle("strikethrough");});
+      }
+    });
   }
 }
 
-var remove = document.getElementById("removeItem"); //create variable for remove button
-
-remove.addEventListener("click", removeElt); //on click for button, run function to remove selected items
 
 
-getList = function () {
-  
-}
+
+
